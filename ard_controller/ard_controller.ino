5f61fe21f46ddd4a079be94739911c903449cd40
Andrew Euredjian <andrew_103@rocketmail.com>
2021-02-01 17:32:00 -0800

    Begin setting up serial processing

--- ard_controller/ard_controller.ino
+++ ard_controller/ard_controller.ino
@@ -1,6 +1,36 @@
 #include <DueTimer.h>
 
+#define PREAMBLE_LENGTH 4
+#define DATA_BYTE_LENGTH 2
+
+enum SerialReadState {
+  INIT,
+  PREAMBLE,
+  
+};
+
+SerialReadState sr_state = INIT;
+
+struct JointAngleStruct {
+  int left_hip;
+  int left_knee;
+  int right_hip;
+};
+
+JointAngleStruct temporary_packet_data;
+JointAngleStruct validated_packet_data;
+
 bool write_flag = false;
+
+void rx_processor() {
+  static int preamble_counter = PREAMBLE_LENGTH;   // Number of ints in the packet preamble
+  static int calculated_checksum = 0xFF; // We have to keep a running checksum of incoming data to verify validity
+  int received_data; // The current int read from the RPi
+
+  if (Serial1.available() > 0) {
+    received_data = Serial1.read();
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
