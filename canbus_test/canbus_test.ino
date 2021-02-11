b60c0ce51b67f50db2ca149c280aebde63e78d53
Andrew Euredjian <andrew_103@rocketmail.com>
2021-02-10 17:55:43 -0800

    gitignore edit and indenting fixes

--- canbus_test/canbus_test.ino
+++ canbus_test/canbus_test.ino
@@ -19,16 +19,16 @@ void setup() {
 }
 
 void print_frame(CAN_FRAME &frame) {
-   Serial.print("ID: 0x");
-   Serial.print(frame.id, HEX);
-   Serial.print(" Len: ");
-   Serial.print(frame.length);
-   Serial.print(" Data: 0x");
-   for (int count = 0; count < frame.length; count++) {
-       Serial.print(frame.data.bytes[count], HEX);
-       Serial.print(" ");
-   }
-   Serial.print("\r\n");  
+  Serial.print("ID: 0x");
+  Serial.print(frame.id, HEX);
+  Serial.print(" Len: ");
+  Serial.print(frame.length);
+  Serial.print(" Data: 0x");
+  for (int count = 0; count < frame.length; count++) {
+    Serial.print(frame.data.bytes[count], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\r\n");  
 }
 
 void loop() {
